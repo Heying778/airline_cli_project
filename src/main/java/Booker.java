f0ca7f9dfccce329214de0034bd1888833dbac4c
Carla022 <cs.morenol@outlook.com>
2022-08-30 17:13:54 +0100

    added enum des

---
+++ src/main/java/Booker.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Booker {
+//    Add a new flight
+
+//    PROPERTIES -------
+    private ArrayList<Flight>flightArrayList;
+
+//    CONSTRUCTOR -------
+    public Booker(){
+
+    addFlights(Flight flight) {
+        this.flightArrayList.add(flight);
+        System.out.println(flight+ " has been added to SunnySide's flight list");
+    }
+
+
+
+//    Display all available flights
+//    Add a new passenger
+//    Book a passenger onto a flight
+//    Cancel a flight
+
+
+}
---
+++ src/main/java/Destination.java
@@ -0,0 +1,31 @@
+public enum Destination {
+
+    //    The objects/values that we can use:
+    MADRID,
+    BALI,
+    BORABORA,
+    BRAZIL,
+    DUBAI,
+    BULGARIA,
+    ITALY,
+    MALDIVES,
+    MOROCCO,
+    NEWYORK,
+    PORTUGAL,
+    MALTA,
+    SYDNEY,
+    CYPRUS;
+
+    public static boolean destinationCheck(String destination) {
+        destination = destination.toUpperCase();
+        for (Destination location : Destination.values()) {
+            if (location.name().equals(destination)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+}
