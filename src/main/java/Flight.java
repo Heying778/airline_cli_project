c8dfc45b73705ff23ad0e2697772bba7bf40a731
Heying778 <yingyinghe2022@gmail.com>
2022-08-30 16:13:12 +0100

    flight file v1

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,28 @@
-public class Flights {
+public class Flight {
+
+    private String destination;
+    private int id;
+    private Passenger passengerCollection;
+
+    public Flight (String destination, int id){
+        this.destination = destination;
+        this.id = id;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -37,7 +37,7 @@ public class Passenger {
 //    public void addPassenger(){
 //        this.addPassenger();
 //    }
-    
+
 
 
 
