f18a2b3e08ea92420434dc62aac18d1dd393f920
Heying778 <yingyinghe2022@gmail.com>
2022-08-30 16:00:26 +0100

    passenger file v1

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -4,6 +4,42 @@ public class Passenger {
     private String contactInformation;
     private int id;
 
+    public Passenger (String name, String contactInformation, int id){
+        this.name = name;
+        this.contactInformation = contactInformation;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+//    public void addPassenger(){
+//        this.addPassenger();
+//    }
+    
+
+
 
 
 }
