720395a782debf9eb72095b622e08fd171036be8
Carla022 <cs.morenol@outlook.com>
2022-08-31 13:01:33 +0100

    unique id generator

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,6 @@
 import java.awt.print.Book;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class Runner {
 
@@ -19,11 +20,14 @@ public class Runner {
             System.out.println("Thank You For Using Our Services.");
         } else {
 
-            System.out.println(" To add a new flight press 1");
-            System.out.println(" To display all available flights press 2");
-            System.out.println(" To add a new passenger press 3");
-            System.out.println(" To book a passenger onto a flight press 4");
-            System.out.println(" To cancel a flight press 5");
+            System.out.println(":::THIS IS A SELF-SERVICE SYSTEM:::");
+            System.out.println("");
+
+            System.out.println(" To Add A New Flight Press 1");
+            System.out.println(" To Display All Available Flights Press 2");
+            System.out.println(" To Add A New Passenger Press 3");
+            System.out.println(" To Book A Passenger Onto A Flight Press 4");
+            System.out.println(" To Cancel A Flight Press 5");
 
             Scanner scanner2 = new Scanner(System.in);
             int action = scanner2.nextInt();
@@ -41,12 +45,24 @@ public class Runner {
 
             } else if (action == 3) {
                 booker.addPassenger();
+                UUID userId = UUID.randomUUID();
+                Thread.sleep(900); // 900ms = 0.9s
+                System.out.println("GENERATING PASSENGER ID...");
+                System.out.println("");
+                Thread.sleep(1000); // 1000ms = 1s
+                System.out.println("PassengerId: " + userId);
+                System.out.println("");
 
             } else if (action == 4) {
                 booker.addPassengerToFlight(booker.getPassengerToAddToFlight(), booker.getFlightToAddPassengerTo());
 
             } else if (action == 5) {
+                System.out.println("Please Key In Your Unique ID Number");
+                Scanner scanner3 = new Scanner(System.in);
+                int PassengerID = Integer.parseInt(scanner1.nextLine());
                 booker.removeFlight();
+                System.out.println(PassengerID + " Flight Has Been Cancelled");
+                System.out.println("");
 
             }
 
