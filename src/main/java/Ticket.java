5e25d895b1da8f3e66865cfebb4a45f7e2a1ab67
Carla022 <cs.morenol@outlook.com>
2022-08-31 16:02:39 +0100

    commented Ticket class

---
+++ src/main/java/Ticket.java
@@ -0,0 +1,38 @@
+public class Ticket {
+
+//    PROPERTIES:::::::::
+//
+//    private String passengernName;
+//    private int passengerId;
+//    private String destination;
+//
+//    public Ticket (String passengerName, int passengerId, String destination ){
+//        this.passengernName = passengerName;
+//        this.passengerId = passengerId;
+//        this.destination = destination;
+//    }
+//
+//    public String getPassengerName() {
+//        return passengernName;
+//    }
+//
+//    public void setPassengerName() {
+//        this.passengernName = passengernName;
+//    }
+//
+//    public int getPassengerId() {
+//        return passengerId;
+//    }
+//
+//    public void setPassengerId(int id) {
+//        this.passengerId = passengerId;
+//    }
+//
+//    public String getDestination() {
+//        return destination;
+//    }
+//
+//    public void setDestination() {
+//        this.destination = destination;
+//    }
+}
