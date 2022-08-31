c8225c17eac8dddcf2ef3f617e5f8d23a422b0b1
Heying778 <yingyinghe2022@gmail.com>
2022-08-31 16:52:26 +0100

    Saver added.

---
+++ src/main/java/Saver.java
@@ -0,0 +1,47 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+
+//files stored on your system and add functionality to
+// store the records of flights and passengers to their
+// own files.
+
+public class Saver {
+    private Flight id;
+    private Flight destination;
+    private Flight passengerList;
+
+    private Passenger name;
+    private Passenger contactEmail;
+    private Passenger contactNumber;
+    private Passenger passengerID;
+
+    public static void saver(Flight id,
+                 Flight destination,
+                 Flight passengerList,
+
+                 Passenger name,
+                 Passenger contactEmail,
+                 Passenger contactNumber,
+                 Passenger passengerID ) {
+
+        String flightData =id+"||"+destination+"||"+passengerList ;//This is the data in the output file
+        String passengerData =passengerID+"||"+name+"||"+contactEmail+"||"+contactNumber;//This is the data in the output file
+
+        try (PrintWriter out = new PrintWriter("flightData.txt")) {
+            out.println(flightData);
+        }
+
+        catch (Exception e) {
+            e.getStackTrace();
+        }
+
+        try (PrintWriter out = new PrintWriter("flightData.txt")) {
+            out.println(passengerData);
+        }
+
+        catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+}
